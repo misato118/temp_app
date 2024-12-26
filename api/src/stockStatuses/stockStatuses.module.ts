c26a2c7914d39790191488a99c2fa0ecd599c396
misato118 <mtsz.29910@gmail.com>
2024-12-26 06:02:33 -0500

    Removed unnecessary files

--- api/src/stockStatuses/stockStatuses.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StockStatusesService } from './stockStatuses.service';
-import { StockStatusesResolver } from './stockStatuses.resolver';
-import { PrismaService } from 'src/prisma.service';
-
-@Module({
-  providers: [StockStatusesResolver, StockStatusesService, PrismaService],
-})
-export class StockStatusesModule {}
