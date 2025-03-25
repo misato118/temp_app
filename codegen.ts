1982d2a63f24cf1f12627b88f9af3906df4182c3
misato118 <mtsz.29910@gmail.com>
2025-03-25 15:53:37 -0400

    Modified codegen to fix duplicate identifier errors

--- codegen.ts
+++ codegen.ts
@@ -1,11 +1,11 @@
 module.exports = {
   overwrite: true,
   schema: "./api/src/schema.gql",
+  documents: "./toc/**/*.graphql",
   generates: {
     "./toc/features/utils/graphql/typeDefs/": {
-      documents: "./toc/**/*.graphql",
       preset: "client",
-      plugins: ["typescript", "typescript-resolvers"],
+      plugins: [],
     },
   },
 };
